402b9f0838182bd89ac74bb838075e5bac1bf8a0
NicolasVelazquez <npzuliani@gmail.com>
2019-01-05 19:37:46 -0300

    primer commit

---
+++ src/main/java/com/nvelazquez/App.java
@@ -0,0 +1,13 @@
+package com.nvelazquez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
